0f5c93ef8e054ad6f31727af6ef37f068725d0c4
Benoit Pierre <benoit.pierre@gmail.com>
2014-02-19 12:57:02 +0100

    archlinux: add directory

---
+++ archlinux/build-and-install.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+set -x
+
+[ -r "$HOME/.makepkg.conf" ] && . "$HOME/.makepkg.conf"
+
+cd "$(dirname "$0")" &&
+. ./PKGBUILD &&
+pkgver="$(git describe --tags | sed -n '/^[0-9.]\+\(-alpha[0-9]\+\)\?-[0-9]\+-[0-9a-z]\+$/{s/-/./g;p;Q0};Q1')" &&
+src="src/$pkgname-$pkgver" &&
+rm -rf src pkg &&
+mkdir -p "$src" &&
+sed "s/^pkgver=.*\$/pkgver=$pkgver/" PKGBUILD >PKGBUILD.tmp &&
+(cd "$OLDPWD" && git ls-files -z | xargs -0 cp -a --no-dereference --parents --target-directory="$OLDPWD/$src") &&
+export PACKAGER="${PACKAGER:-`git config user.name` <`git config user.email`>}" &&
+echo -n "`git describe`" > "$src/.version_stamp" &&
+makepkg --noextract --force -p PKGBUILD.tmp &&
+rm -rf src pkg PKGBUILD.tmp &&
+sudo pacman -U --noconfirm "$pkgname-$pkgver-$pkgrel-`uname -m`${PKGEXT:-.pkg.tar.xz}"
+
